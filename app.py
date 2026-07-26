4e50e2844867080eeb99910e0844ac3897c01cf1
mayaannkkk <goyalmayank492@gmail.com>
2026-07-27 01:01:16 +0530

    Added a FastApi File

---
+++ app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import sklearn
+from model.predict import predict_output, model, MODEL_VERSION
+from schema.user_input import UserInput
+from schema.prediction_response import PredictionResponse
+
+app = FastAPI()
+
+# Human readable
+@app.get('/')
+def home():
+    return {"message" : "Insurance Domain API"}
+
+# Machine Readable
+@app.get('/health')
+def health_checkup():
+    return {
+        "Status" : "OK",
+        "version" : MODEL_VERSION,
+        "model_loaded" : model is not None
+    }
+
+@app.post('/predict', response_model = PredictionResponse)
+def predict_premium(data : UserInput):
+
+    input_df = {
+        'bmi' : data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }
+    try:
+        prediction = predict_output(input_df)
+
+        return JSONResponse(status_code=200, content={"response": prediction})
+    
+    except Exception as e:
+
+        return JSONResponse(status_code=500, content=str(e))
